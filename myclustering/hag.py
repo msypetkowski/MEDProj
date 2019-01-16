86135917e3aca6aab6197005503b6cd907905dee
Michał Sypetkowski <M.Sypetkowski@gmail.com>
2019-01-17 00:09:02 +0100

    Optimize (coefficient) HAG implementation

--- myclustering/hag.py
+++ myclustering/hag.py
@@ -3,6 +3,13 @@ Author: Michał Sypetkowski
 
 Implementation of Hierarchical Agglomerative Clustering
 (https://en.wikipedia.org/wiki/Hierarchical_clustering)
+
+Implemented is variant with time complexity of O(n^3), and memory O(n^2).
+O(n^2*log(n)) version optimize minimum index finding with priority queue.
+Here, hhis operation has relatively low time coefficient, because the method is
+implemented in C++ in numpy. Moreover, it would increase memory coefficient.
+Therefore O(n^2*log(n)) implementation may not improve the performance
+very significantly in this implementation -- python (interpreter) overhead is high.
 """
 
 import numpy as np
@@ -33,31 +40,27 @@ class MyHAGClustering:
         clusters = [Cluster((i, row), distances) for i, row in enumerate(data)]
         for c1 in clusters:
             for c2 in clusters:
-                if c1 is not c2:
+                if c1.id < c2.id:
                     distances[c1.id, c2.id] = np.linalg.norm(c1.points[0][1] - c2.points[0][1])
-                    distances[c2.id, c1.id] = distances[c1.id, c2.id]
         distances_sum = distances.copy()
-        # print(distances[:5,:3])
 
         while sum([c.valid for c in clusters]) > self.n_clusters:
             m = np.unravel_index(distances.argmin(), distances.shape)
-            # print(m)
             c1 = clusters[m[0]]
             c2 = clusters[m[1]]
             assert(c1.points)
             assert(c2.points)
             for c in clusters:
                 if c.valid and c is not c1 and c is not c2:
-                    distances_sum[c1.id, c.id] = distances_sum[c1.id, c.id] + distances_sum[c2.id, c.id]
-                    distances_sum[c.id, c1.id] = distances_sum[c1.id, c.id]
-                    distances[c1.id, c.id] = distances_sum[c1.id, c.id] / len(c1.points) / len(c2.points)
-                    distances[c.id, c1.id] = distances[c1.id, c.id]
+                    ids = [c1.id, c.id]
+                    i1, i2 = min(ids), max(ids)
+                    distances_sum[i1, i2] = distances_sum[i1, i2] + distances_sum[min(c2.id,c.id), max(c2.id, c.id)]
+                    distances[i1, i2] = distances_sum[i1, i2] / len(c1.points) / len(c2.points)
             for c in clusters:
                 if c.valid:
-                    distances_sum[c.id, c2.id] = float("inf")
-                    distances_sum[c2.id, c.id] = float("inf")
-                    distances[c.id, c2.id] = float("inf")
-                    distances[c2.id, c.id] = float("inf")
+                    ids = [c.id, c2.id]
+                    i1, i2 = min(ids), max(ids)
+                    distances[i1, i2] = float("inf")
             c1.merge(c2)
 
         ret = np.zeros(len(data), dtype=int)
@@ -68,5 +71,4 @@ class MyHAGClustering:
                     ret[i] = id
                 id += 1
         self.labels_ = ret
-        # print(ret)
         return ret
